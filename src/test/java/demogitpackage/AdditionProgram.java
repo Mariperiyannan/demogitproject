753240564b1afb078c804fa952f580c0d9e098c1
Mariperiyannan <mariperya44@gmail.com>
2024-11-22 17:12:32 +0530

    create demo git project

---
+++ src/test/java/demogitpackage/AdditionProgram.java
@@ -0,0 +1,12 @@
+package demogitpackage;
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+      int i=20;
+      int j=30;
+      int c=i+j;
+      System.out.println("result "+c);
+	}
+
+}
